fb9359d228fbcc88de3484f84e9be70f35c4b5a8
Taghreed Magdy-Sorour <Taghreed.Magdy-Sorour@vodafone.com.eg>
2024-10-29 14:28:21 +0300

    Fix Class instantiate issue

--- SDET-Unicorns-API/src/test/java/com/sdetunicorns/api/tests/Brands.java
+++ SDET-Unicorns-API/src/test/java/com/sdetunicorns/api/tests/Brands.java
@@ -2,7 +2,6 @@ package com.sdetunicorns.api.tests;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
-import static org.hamcrest.core.Is.is;
 import com.sdetunicorns.api.properties_reading.ReadProperties;
 import java.io.IOException;
 import java.util.Properties;
@@ -13,28 +12,32 @@ import org.testng.annotations.Test;
 
 public class Brands {
 
-    Properties properties;
+    Properties properties = ReadProperties.setProperties();;
     String brandApi = properties.getProperty("base_url").concat("/brands");
     String brandHasID;
     String brandHasName;
+
+    public Brands() throws IOException {
+    }
 
 
     @BeforeTest
     public void setBaseUri() throws IOException {
         RestAssured.baseURI = brandApi;
-        properties = ReadProperties.setProperties();
         brandHasID = properties.getProperty("brand_id");
         brandHasName = properties.getProperty("brand_name");
     }
 
 
+
+
     @Test
-    public void validateBrandResponseStatus(){
+    public void validateBrandResponseStatus() {
         given().when().get().then().assertThat().statusCode(200);
     }
 
     @Test
-    public void validateBrandsListEntries(){
+    public void validateBrandsListEntries() {
         get().then().assertThat()
                 .body("size()", greaterThan(1));
 
@@ -44,4 +47,6 @@ public class Brands {
                 .body("",everyItem(hasKey(brandHasName)));
     }
 
-}
+    }
+
+
